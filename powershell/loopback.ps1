42024aa6fbae5589d3275f35a95c5a87ae8fd8eb
batkiz <batkiz@outlook.com>
2019-12-20 18:52:10 +0800

    refactor uwp loopback ps1 script

--- powershell/loopback.ps1
+++ powershell/loopback.ps1
@@ -1,15 +1,14 @@
 # a powershell script to enable local loopback for uwp apps
 
-Get-ChildItem "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Mappings" |
-ForEach-Object { 
-    Write-Host -ForegroundColor Yellow "Enable uwp loopback..." } {
-    # $values = Get-ItemProperty $_.PSPath;
-    # $uwpDisplayName = $values.DisplayName
+Write-Host "Enable all UWP apps' loopback"
+
+$apps = Get-ChildItem "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Mappings"
+
+$apps | ForEach-Object {
     $arr = ($_.Name -split "\\")
     $uwpSid = $arr[$arr.count - 1]
-    # $uwpSid
-    # Write-Output "$uwpDisplayName  $uwpSid"
 
     CheckNetIsolation.exe loopbackexempt -a -p="$uwpSid" | Out-Null
+}
 
-} { Write-Host -ForegroundColor Yellow "Finished!" }
+Write-Host "Finished!" 
