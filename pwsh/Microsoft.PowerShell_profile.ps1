f33b25ce2abb0e883fb6c8e775e7fa60d041d6f0
batkiz <batkiz@outlook.com>
2020-11-03 11:49:21 +0800

    update

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -39,13 +39,19 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 # winget tab completion
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
-        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $Local:word = $wordToComplete.Replace('"', '""')
-        $Local:ast = $commandAst.ToString().Replace('"', '""')
-        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
 }
+
+
+# several cli completions
+(& deno completions powershell) | Out-String | Invoke-Expression
+(& rustup completions powershell) | Out-String | Invoke-Expression
+(& gh completion -s powershell) | Out-String | Invoke-Expression
 
 # use nvim in wsl
 function dos2nix {
