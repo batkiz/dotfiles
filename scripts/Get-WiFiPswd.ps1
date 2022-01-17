1cfd5b4041339e546c77f2f587afa46ef63ef647
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-17 15:54:36 +0800

    Delete scripts directory

--- scripts/Get-WiFiPswd.ps1
+++
@@ -1,7 +0,0 @@
-$WirelessSSIDs = (netsh wlan show profiles | Select-String ': ' ) -replace ".*:\s+"
-$WifiInfo = foreach($SSID in $WirelessSSIDs) {
-    $Password = (netsh wlan show profiles name=$SSID key=clear | Select-String 'Key Content') -replace ".*:\s+"
-        New-Object -TypeName psobject -Property @{"SSID"=$SSID;"Password"=$Password}
-
-}  
-$WifiInfo | ConvertTo-Json
